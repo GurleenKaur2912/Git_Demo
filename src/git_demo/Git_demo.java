d660e0521201c99a4ff9dfdcea77f986527d8403
GurleenKaur2912 <gurleenjammu2912@gmail.com>
2024-01-24 09:03:01 -0500

    Just added a comment

--- src/git_demo/Git_demo.java
+++ src/git_demo/Git_demo.java
@@ -17,6 +17,7 @@ public class Git_demo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        //fetch and merge performed
         
     }
     
